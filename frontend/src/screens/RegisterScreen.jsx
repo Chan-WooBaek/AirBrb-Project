c96abd5b317482a0893f5fd572f736bd26f6297d
Chan-WooBaek <chanwoobaek@outlook.com>
2021-11-11 21:06:16 +1100

    Implemented logout button refresh on listing route, added passwords matching text

--- frontend/src/screens/RegisterScreen.jsx
+++ frontend/src/screens/RegisterScreen.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import myFetch from '../components/fetcher';
 
 const RegisterForm = (e) => {
-  console.log(e);
-  if (e.password !== e.confirmPassword) {
-    console.log('Passwords dont match');
-    return;
-  }
+  console.log();
   const body = {
     email: e.email,
     password: e.password,
@@ -14,9 +10,12 @@ const RegisterForm = (e) => {
   }
   myFetch('POST', 'user/auth/register', null, body)
     .then((data) => {
+      localStorage.setItem('token', data.token);
+      localStorage.setItem('isLoggedIn', true);
       console.log('Successfully registered new user');
     })
     .catch((data) => {
+      alert('Email already registered');
       console.log('Not successful in registering new user');
     })
 }
@@ -26,12 +25,18 @@ const RegisterScreen = () => {
   const [password, setPassword] = React.useState('');
   const [confirmPassword, setConfirmPassword] = React.useState('');
   const [name, setName] = React.useState('');
+
+  if (password !== confirmPassword) {
+    console.log('Passwords dont match');
+  }
+
   return (
     <div>
       <h1>Register Form</h1>
       Email<input type="text" onChange={(e) => setEmail(e.target.value)} /><br/>
       Password<input type="text" onChange={(e) => setPassword(e.target.value)} /><br/>
       ConfirmPassword<input type="text" onChange={(e) => setConfirmPassword(e.target.value)} /><br/>
+      <div>{password !== confirmPassword ? "passswords don't match" : 'passwords match'}</div>
       Name<input type="text" onChange={(e) => setName(e.target.value)} /><br/>
       <button onClick={() => RegisterForm({ email: email, password: password, confirmPassword: confirmPassword, name: name })}>Submit</button>
     </div>
