ab7701e1c1f9ea1a7766eb86f76f7561d0a3a531
rkouch <raymond-kouch@hotmail.com>
2021-11-13 13:25:56 +1100

    create listing screen

--- frontend/src/screens/CreateListingScreen.jsx
+++ frontend/src/screens/CreateListingScreen.jsx
@@ -10,6 +10,9 @@ import DialogTitle from '@mui/material/DialogTitle';
 
 const createListing = (prop) => {
   console.log(prop.title);
+  if (prop.title === '' || prop.address === {} || prop.price === '' || prop.thumbnail === '' || prop.metadata === {}) {
+    alert('Please fill in all blank spaces')
+  }
   const body = {
     title: prop.title,
     address: prop.address,
@@ -37,13 +40,17 @@ const CreateListingForm = () => {
   };
 
   const [title, setTitle] = React.useState('');
-  const [address, setAddress] = React.useState('');
+  const [street, setStreet] = React.useState('');
+  const [city, setCity] = React.useState('');
+  const [postcode, setPostcode] = React.useState(0);
+  const [state, setState] = React.useState('');
   const [price, setPrice] = React.useState(0);
   const [thumbnail, setThumbnail] = React.useState('');
-  const [bathrooms, setBathrooms] = React.useState('');
+  const [bathrooms, setBathrooms] = React.useState(0);
   const [propType, setPropType] = React.useState('');
   const [amenities, setAmenities] = React.useState('');
-
+  const [beds, setBeds] = React.useState(0);
+  // const [bedrooms, setBedrooms] = React.useState([''])
   return (
     <div>
       <Button variant="outlined" onClick={handleClickOpen}>
@@ -53,13 +60,14 @@ const CreateListingForm = () => {
         <DialogTitle>Create New Listing</DialogTitle>
         <DialogContent>
           <DialogContentText>
+            Please fill out all required fields to create new listing
           </DialogContentText>
           <TextField
             autoFocus
             margin="dense"
             id="title"
             label="Title"
-            type="email"
+            type="text"
             fullWidth
             variant="standard"
             onChange={(e) => setTitle(e.target.value)}
@@ -67,19 +75,49 @@ const CreateListingForm = () => {
           <TextField
             autoFocus
             margin="dense"
-            id="address"
-            label="Address"
-            type="email"
+            id="street"
+            label="Street"
+            type="text"
             fullWidth
             variant="standard"
-            onChange={(e) => setAddress(e.target.value)}
+            onChange={(e) => setStreet(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="city"
+            label="City"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="state"
+            label="State"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setState(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="postcode"
+            label="Postcode"
+            type="number"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setPostcode(e.target.value)}
           />
           <TextField
             autoFocus
             margin="dense"
             id="price"
-            label="Price"
-            type="email"
+            label="Price (per night)"
+            type="number"
             fullWidth
             variant="standard"
             onChange={(e) => setPrice(e.target.value)}
@@ -89,7 +127,7 @@ const CreateListingForm = () => {
             margin="dense"
             id="thumbnail"
             label="Thumbnail"
-            type="email"
+            type="text"
             fullWidth
             variant="standard"
             onChange={(e) => setThumbnail(e.target.value)}
@@ -97,9 +135,9 @@ const CreateListingForm = () => {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="bathrooms"
             label="No. of bathrooms"
-            type="email"
+            type="number"
             fullWidth
             variant="standard"
             onChange={(e) => setBathrooms(e.target.value)}
@@ -107,9 +145,9 @@ const CreateListingForm = () => {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="propType"
             label="Property type"
-            type="email"
+            type="text"
             fullWidth
             variant="standard"
             onChange={(e) => setPropType(e.target.value)}
@@ -117,22 +155,43 @@ const CreateListingForm = () => {
           <TextField
             autoFocus
             margin="dense"
-            id="name"
+            id="amenities"
             label="Property amenities"
-            type="email"
+            type="text"
             fullWidth
             variant="standard"
             onChange={(e) => setAmenities(e.target.value)}
           />
+          <TextField
+            autoFocus
+            margin="dense"
+            id="beds"
+            label="No. of beds"
+            type="number"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setBeds(e.target.value)}
+          />
         </DialogContent>
         <DialogActions>
+          {/* <Button onClick={}>Add Bedroom</Button> */}
           <Button onClick={handleClose}>Cancel</Button>
           <Button onClick={() => createListing({
             title: title,
-            address: { address: address },
+            address: {
+              street: street,
+              city: city,
+              state: state,
+              postcode: postcode,
+            },
             price: price,
             thumbnail: thumbnail,
-            metadata: { bathrooms: bathrooms, propType: propType, amenities: amenities }
+            metadata: {
+              bathrooms: bathrooms,
+              beds: beds,
+              propType: propType,
+              amenities: amenities
+            }
           })}>
             Create
           </Button>
