962e40af540f505f0e38f6f2099760103272c345
Chan-WooBaek <chanwoobaek@outlook.com>
2021-11-18 22:33:51 +1100

    Finished search filter excluding reviews

--- frontend/src/screens/ListingsScreen.jsx
+++ frontend/src/screens/ListingsScreen.jsx
@@ -11,19 +11,33 @@ ListingsScreen.propTypes = {
   setSearchString: PropTypes.func,
 }
 
-export default function ListingsScreen ({ isLoggedIn, setLoggedIn, searchString, setSearchString }) {
+export default function ListingsScreen ({ isLoggedIn, setLoggedIn }) {
+  const [searchString, setSearchString] = React.useState('');
+  const [minBedrooms, setMinBedrooms] = React.useState(null);
+  const [maxBedrooms, setMaxBedrooms] = React.useState(null);
+  const [minPrice, setMinPrice] = React.useState(null);
+  const [maxPrice, setMaxPrice] = React.useState(null);
+  const [dateRange, setDateRange] = React.useState([null, null]);
   return (
     <div>
       {isLoggedIn
         ? <>
-            <LoggedInAppBar isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn} searchString={searchString} setSearchString={setSearchString}></LoggedInAppBar>
-            <ListingsDisplay searchString={searchString} setSearchString={setSearchString}/>
+            <LoggedInAppBar isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn}
+              searchString={searchString} setSearchString={setSearchString}
+              isListingDisplay={true}
+              minBedrooms={minBedrooms} setMinBedrooms={setMinBedrooms}
+              maxBedrooms={maxBedrooms} setMaxBedrooms={setMaxBedrooms}
+              minPrice={minPrice} setMinPrice={setMinPrice}
+              maxPrice={maxPrice} setMaxPrice={setMaxPrice}
+              dateRange={dateRange} setDateRange={setDateRange}
+            >
+            </LoggedInAppBar>
           </>
         : <>
-            <GuestAppBar></GuestAppBar>
-            <ListingsDisplay/>
+            <GuestAppBar searchString={searchString} setSearchString={setSearchString} isListingDisplay={true}></GuestAppBar>
           </>
       }
+      <ListingsDisplay searchString={searchString} setSearchString={setSearchString} minBedrooms={minBedrooms} maxBedrooms={maxBedrooms} minPrice={minPrice} maxPrice={maxPrice} dateRange={dateRange}/>
     </div>
   );
 }
